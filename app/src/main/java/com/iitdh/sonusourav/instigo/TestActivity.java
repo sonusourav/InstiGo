52a5b1ccee095c2533400d4b4c329fb3c0abc5f2
sonus <sonusouravdx001@gmail.com>
2018-08-23 23:40:15 +0530

    Registration pages done.

---
+++ app/src/main/java/com/iitdh/sonusourav/instigo/TestActivity.java
@@ -0,0 +1,12 @@
+package com.iitdh.sonusourav.instigo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
